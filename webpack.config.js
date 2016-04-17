cbf1a4ad3f084b7b73332f83ebe80d7eda7afc94
Brandon Braun <brandon.braun@gmail.com>
2016-04-16 20:51:26 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+module.exports = {
+  entry: {
+    app: PATHS.app
+  },
+
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js'
+  },
+
+  devServer: {
+    contentBase: PATHS.build,
+    historyApiFallback: true,
+    hot: true,
+    inline: true,
+    progress: true,
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  resolve: {
+    extensions: [
+      '',
+      '.js',
+      '.jsx'
+    ]
+  },
+
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
+      }
+    ]
+  }
+};
